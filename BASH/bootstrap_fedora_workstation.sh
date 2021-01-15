ed53056fd6732f336d204331ea9c861003d3ac51
William Whinn <w.willis.1986@gmail.com>
2021-01-15 09:52:22 +0000

    Include metadata

--- BASH/bootstrap_fedora_workstation.sh
+++ BASH/bootstrap_fedora_workstation.sh
@@ -20,6 +20,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+############
+# Metadata #
+############
+
+# Author:  William Whinn
+# Version: 0.0.1
+# Date:    15th January 2021
+
 ###############
 # Description #
 ###############
